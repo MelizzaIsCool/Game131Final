296f81c0dab0f14e97163ad921b2cb08039b556b
svc-student <svc-student@student.directory.jccc>
2018-05-17 21:00:03 -0500

    Final

---
+++ Assets/Editor/Addpoints.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(WhenToMove))]
+public class Addpoints :Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        WhenToMove myScript = (WhenToMove)target;
+
+        if (GUILayout.Button("Add Point"))
+        {
+            myScript.NextMove();
+        }
+
+        if (GUILayout.Button("Remove Last Point"))
+        {
+            myScript.RemovePoint();
+        }
+    }
+}
---
+++ Assets/WhenToMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ExecuteInEditMode]
+public class WhenToMove : MonoBehaviour
+{
+    [Header("The distance you need to be within...")]
+    public float withinRange;
+    [Header("...from this position...")]
+    public Transform startPoint;
+    [Header("...heading in this direction.")]
+    public Transform endPoint;
+    private MoveInstructions addPoints;
+
+
+    // Update is called once per frame
+    public void NextMove ()
+    {
+        addPoints = GameObject.Find("MoveBot").GetComponent<MoveInstructions>();
+        addPoints.positions.Add(startPoint.position);
+        addPoints.ranges.Add(withinRange);
+        addPoints.directions.Add(endPoint.position - startPoint.position);
+	}
+
+    public void RemovePoint()
+    {
+            addPoints = GameObject.Find("MoveBot").GetComponent<MoveInstructions>();
+            addPoints.positions.RemoveAt(addPoints.positions.Count - 1);
+            addPoints.ranges.RemoveAt(addPoints.ranges.Count - 1);
+            addPoints.directions.RemoveAt(addPoints.directions.Count - 1);
+    }
+}
